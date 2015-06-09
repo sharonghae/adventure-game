5f510cbdf0a5616113b7e4e6a2237ed5d454a16a
Zeke Nierenberg <hampzan09@gmail.com>
2015-06-08 22:23:11 -0400

    player written

--- game.source.js
+++ game.source.js
@@ -4,16 +4,14 @@ var Game = require('./game')
 
 var g = new Game()
 
-g.addNode(‘name’, ’Hello adventurer, what is your name’)
 
-g.addNode(‘direction’, ‘Welcome %name%. Do you choose to go left, or right?’)
+g.addNode('direction', 'Welcome. Do you choose to go left, or right?')
 
-g.addNode(‘leftResp’, ‘Ah, good choice %name%. Good choice’)
+g.addNode('leftResp', 'Ah, good choice %name%. Good choice')
 
-g.addNode(‘rightResp’, ‘There be dragons there.’)
+g.addNode('rightResp', 'There be dragons there.')
 
-g.connect(‘name’, ‘direction’) // with no third argument, it always connects!
-g.connect(‘direction’, ‘leftResp’, ‘left’)
-g.connect(‘direction’, ‘rightResp’, ‘right’)
+g.connect('direction', 'leftResp', 'left')
+g.connect('direction', 'rightResp', 'right')
 
 module.exports = g
---
+++ player.js
@@ -0,0 +1,31 @@
+var inquirer = require('inquirer')
+
+var game = require('./game.source')
+
+var currentNode = game.startingPoint
+
+var play = function(node) {
+
+  var choices = node.getConnectionStrings()
+
+  // we've reached a terminal node
+  if(!choices.length) return console.log(node.text)
+
+  inquirer.prompt([{
+    type: 'list',
+    name: node.title,
+    message: node.text,
+    choices: choices
+  }], function(answers) {
+    var answer = answers[node.title]
+    var nextNode = node.route(answer)
+    if(!nextNode) {
+      // console.log(nextNode.text)
+      // console.log('game over!')
+    } else {
+      play(nextNode)
+    }
+  })
+}
+
+play(currentNode)
