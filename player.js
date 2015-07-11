47de82f68825aa62b582b8d4af7c11aa0c2bd1f2
Nimit Maru <nimit.maru@gmail.com>
2015-07-11 16:05:10 -0400

    Update player.js

--- player.js
+++ player.js
@@ -8,7 +8,7 @@ This file has no test specs. It might be a tricky one. You need to look at
 the docs for the inquirer npm package, and see if you can figure out how 
 to make the game run!
 
-If you're running out of time, check out my solution to the problem:
+If you're running out of time, check out our solution to the problem:
 https://gist.github.com/zekenie/e90faf30a789d65c6459
 
 */
